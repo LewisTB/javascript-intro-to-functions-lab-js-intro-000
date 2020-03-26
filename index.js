f4f677bb1d8f6f05985d763139f439b6b80981d9
Lewis Turner-Brown <lewis.tb@hotmail.co.uk>
2020-03-26 15:44:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ function whisper(string) {
 
 
 function sayHiToGrandma(string) {
-  if (`string`)"hello" === lowercase {
+  if (`string`)"hello" === lowercase 
     return "I can't hear you!"
-  }
-  else if (`string`)"HELLO!"===uppercase {
+  
+  else if (`string`)"HELLO!"===uppercase 
     return "YES INDEED!"
-  }
+
   else if (`string`)"I love you, Grandma."
   return "I love you, too."
 }
