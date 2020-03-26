b602236810853fb09c572cf636d418e132574c6b
Lewis Turner-Brown <lewis.tb@hotmail.co.uk>
2020-03-26 15:20:50 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,40 @@
+function shout(string) {
+  return string. toUpperCase()
+}
+
+function whisper(string) {
+  return string. toLowerCase()
+}
+
+// function logShout(string) {
+//   console.log() .toUpperCase()
+// }
+//
+// function logWhisper(string) {
+//   console.log(`string`) .toLowerCase()
+// }
+
+var uppercase = "HELLO"
+
+uppercase.toUpperCase() === uppercase
+
+var lowercase = 'hello'
+
+lowercase.toLowerCase() === lowercase
+
+var mixedCase = 'I love you, Grandma'
+
+mixedCase.toLowerCase() === mixedCase
+
+mixedCase.toUpperCase() === mixedCase
+
+function sayHiToGrandma(string) {
+  if ("hello".lowercase) {
+    return "I can't hear you!"
+  }
+  else if ("HELLO".uppercase) {
+    return "YES INDEED!"
+  }
+  else if (`string`)"I love you, Grandma."
+  return "I love you, too"
+}
